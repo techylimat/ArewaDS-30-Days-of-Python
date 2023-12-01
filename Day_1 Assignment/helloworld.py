0bce23c07c7121ffe2c2d5a4827f95732dae4cae
techylimat <musahalimat045@gmail.com>
2023-12-01 11:32:57 +0000

    helloworld.ipynb

---
+++ Day_1 Assignment/helloworld.py
@@ -0,0 +1,30 @@
+import sys
+print(sys.version)
+print(3 + 4)
+print(4 - 3)
+print(4 % 3)
+print(4 / 3)
+print(4 ** 3)
+print(4 // 3)
+print('Halimat')
+print('Muhammadurabiu')
+print('Nigeria')
+print('I am enjoying 30 days of python')
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type('Halimat'))
+print(type('Muhammadurabiu'))
+print(type('Nigeria'))
+Integer = 5, 300, 300000
+Float = 4.8, 19.7, 200.9
+Complex = 13.78j
+String = 'Lamp', 'Mango'
+Boolean = False
+List = [2, 'Nice', 2.8]
+Tuple = ('Excellent', 'Lion')
+Set = { 'Rice', 15, 2.5}
+Dictionary = { 'Gender' : 'Female', 'Size': 10}
+EU = (((2 - 10)) ** 2) + (((3 - 8)) ** 2) * 1/2
